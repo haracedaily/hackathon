b6c6ea605a99770d25fc27c668637ec6bc336f38
JH\nj700 <nj7001@naver.com>
2025-07-16 20:36:17 +0900

    no message

--- src/pages/JH/Autho.jsx
+++ src/pages/JH/Autho.jsx
@@ -4,7 +4,7 @@ function Autho({isTablet}) {
     //isTablet max-width 960이면 true
     return (
         <>
-        <h1 className={"text-5xl"}>권한 설정 페이지</h1>
+        <h1 className={"text-5xl"}>권한 설정</h1>
         </>
     );
 }
